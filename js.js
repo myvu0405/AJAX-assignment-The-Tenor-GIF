66616b2e271c263e0d8bfb354db42ae2385c2139
My Vu <ngamy.menses.vu@gmail.com>
2022-01-02 00:07:51 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,36 @@
+const button= document.getElementById('searchBtn');
+const subject= document.getElementById('subject');
+const num = document.getElementById('number');
+const display=document.getElementById('tenorDisplay');
+const my_key='PHV1INB3YF7E';
+
+button.addEventListener('click', tenorSearch);
+
+function tenorSearch()
+{
+    let xhr = new XMLHttpRequest();
+    let my_subject = subject.value;
+    let my_number = num.value;
+    
+    xhr.open('GET', `https://api.tenor.com/v1/search?q=${my_subject}&key=${my_key}&limit=${my_number}`, true);
+
+    xhr.onload = function (){
+        if(xhr.status === 200){
+            
+            let responseResult= JSON.parse(xhr.responseText);
+            let responseTenors=responseResult.results;           
+           
+            display.innerHTML='';
+            responseTenors.forEach ((tenor)  => {
+                
+                display.innerHTML+=`<img src=${tenor.media[0].gif.url} class='gif'>`;
+                
+            });
+        } else if (xhr.status === 404){
+            console.log('Content not found');
+        }
+    };
+
+    xhr.send();
+}
+
