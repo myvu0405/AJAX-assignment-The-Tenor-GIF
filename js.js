c642ff5b06cd548022ba32bb71b4c3cff54f2f18
My Vu <ngamy.menses.vu@gmail.com>
2022-01-06 21:42:33 +0100

    updated error handling and style for error message

--- js.js
+++ js.js
@@ -10,10 +10,14 @@ function tenorSearch()
 {
     
     let my_subject = subject.value;
-    let my_number = num.value;
+    let my_number = parseInt(num.value);
     
-    if (my_subject=='' || my_number=='') {
-        display.innerHTML="<p class='error'>Please enter a subject and a number!</p>";
+    //error handling for user input
+    if (my_subject=='') {
+        display.innerHTML="<p class='error'>Please enter a subject!</p>";
+        return;
+    } else if (isNaN(my_number) || my_number<1 || my_number>50){
+        display.innerHTML="<p class='error'>Please enter a valid number between 1-50!</p>";
         return;
     }
     /*
@@ -24,34 +28,58 @@ function tenorSearch()
         if(xhr.status === 200){
             
             let responseResult= JSON.parse(xhr.responseText);
-            let responseTenors=responseResult.results;           
-           
+            let responseTenors=responseResult.results;       
+            
+            //if no data/gif returns
+            if(responseTenors.length===0) {
+
+                display.innerHTML="<p class='error'>No data returns. Please try your search again!</p>";
+                return;
+            }
+            
             display.innerHTML='';
             responseTenors.forEach ((tenor)  => {
                 
-                display.innerHTML+=`<img src=${tenor.media[0].gif.url} class='gif'>`;
+                display.innerHTML+=`<img src='${tenor.media[0].gif.url}' class='gif img-thumbnail'>`;
                 
             });
+            //erase/reset the search form
+            subject.value='';
+            num.value='';
         } else if (xhr.status === 404){
-            console.log('Content not found');
+            display.innerHTML="<p class='error'>Error occurs. Please try your search again!</p>";
+            
+            //console.log('Content not found');
         }
     };
 
     xhr.send();
     */
-   fetch(`https://api.tenor.com/v1/search?q=${my_subject}&key=${my_key}&limit=${my_number}`)
-   .then(res=>res.json())
-   .then(data => {
-       let responseTenors=data.results
-       display.innerHTML='';
-            responseTenors.forEach ((tenor)  => {
+     
+    fetch(`https://api.tenor.com/v1/search?q=${my_subject}&key=${my_key}&limit=${my_number}`)
+    .then(res=>res.json())
+    .then(data => {
+        let responseTenors=data.results;
+        //if no data/gif returns
+        if(responseTenors.length===0) {
+
+            display.innerHTML="<p class='error'>No data returns. Please try your search again!</p>";
+            return;
+        }
+        //if any data returns ==> show data
+        display.innerHTML='';
+        responseTenors.forEach ((tenor)  => {
                 
-                display.innerHTML+=`<img src=${tenor.media[0].gif.url} class='gif img-thumbnail'>`;
+                display.innerHTML+=`<img src='${tenor.media[0].gif.url}' class='gif img-thumbnail'>`;
                 
-            });
+        });
+        //erase the search form
+        subject.value='';
+        num.value='';
     })
-   .catch((error) => {
-    console.error('Error:', error);
-  })
+    .catch((error) => {
+        display.innerHTML=`<p class='error'>Error occurs: ${error}. Please try your search again!</p>`;
+        //console.error('Error:', error);
+    })
 }
 
